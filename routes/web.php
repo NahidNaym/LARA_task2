ab470c60078a76a092c7b2348a73d247b33c7b94
NahidNaym <115368515+NahidNaym@users.noreply.github.com>
2024-07-28 17:29:47 +0300

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,79 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-// Route::get('first', function () {
-//     return 'welcome to first Route';
-// });
-
-// Route::get('cars/{id?}', function ($id=10) {
-//     return 'car number is  '.$id;
-// });
-// // Route::get('vendor/{id?}' ,function ($id=0) {
-// //     return 'vendor id is   ' .$id;
-// // })-> where (['id'=> '[0-9]+']);
-// Route::get('vendor/{id?}' ,function ($id=0) {
-//     return 'vendor id is   ' .$id;
-// })-> wherenumber ('id');
-
-// Route::get('user/{name}/{age?}' ,function ($name,$age=0) {
-//     if( $age!=0)
-//        return 'User name is   ' .$name . '   and age is' . $age ;
-//     else   return 'User name is   ' .$name ;
-     
-// })-> where (['age'=> '[0-9]+'] ,
-// ['name'=> '[a-zA-Z]+']);
-// Route::get('grade/{degree}' ,function ($degree=0) {
-//     return 'grade id is   ' .$degree;
-// })-> whereIn ('degree' ,['good','verygood']);
+use App\Http\Controllers\ExampleController;
+use App\Http\Controllers\CarController;
+use App\Http\Controllers\ClassController;
 
 
-Route::prefix('company')->group(function() {
-    Route::get('', function () {
-        return 'company index  ';
-    });
-    Route::get('it', function () {
-        return 'company it  ';
-    });
-});
+Route::get('cars', [CarController::class, 'index']);
+Route::get('cars/create', [CarController::class, 'create']);
+Route::post('cars', [CarController::class, 'store'])->name('cars.store');
+Route::get('cars/{id}/edit', [CarController::class, 'edit'])->name('cars.edit');
+Route::put('cars/{id}', [CarController::class, 'update'])->name('cars.update');
+Route::get('cars/{id}/delete', [CarController::class, 'destroy'])->name('cars.destroy');
 
 
-
-Route::prefix('accounts')->group(function() {
-    Route::get('', function () {
-        return 'ACCOUNTS   ';
-    });
-    Route::get('admin', function () {
-        return 'ACCOUNTS ADMIN  ';
-    });
-    Route::get('user', function () {
-        return 'ACCOUNTS USER  ';
-    });
-});
-
-Route::prefix('cars')->group(function(){
-    Route::prefix('usa')->group(function(){
-        Route::get('ford', function () {
-            return 'FORD  ';
-        });
-        Route::get('tesla', function () {
-            return 'TESLA  ';
-        });
-    });
-
-    Route::prefix('ger')->group(function() {
-        Route::get('mercedes', function () {
-            return 'MERCEDES  caRs ';
-        });
-        Route::get('audi', function () {
-            return 'AUDI  caRs';
-        });
-        Route::get('volkswagen', function () {
-            return 'volkswagen caRs  ';
-        });
-    });
-    
-});
+// get: show data
+// post: submit new data to server
+// put: update data in server
+// patch
+// delete: 
+Route::get('classes', [ClassController::class, 'index']);
+Route::get('classes/create', [ClassController::class, 'create']);
+Route::post('classes', [ClassController::class, 'store'])->name('classes.store');
+Route::get('classes/{id}/edit', [ClassController::class, 'edit'])->name('classes.edit');
+Route::put('classes/{id}', [ClassController::class, 'update'])->name('classes.update');
+Route::get('classes/{id}/delete', [ClassController::class, 'destroy'])->name('classes.destroy');
+Route::get("show/{id}", [ClassController::class,"show"])->name('classes.show');
